8e7e12cee82628728f32f48192e7e9a880a9cf5a
Marko Miter <mitermarko@gmail.com>
2025-08-27 20:26:37 +0200

    Create Event class, create Event class properties

---
+++ Classes/Event.cs
@@ -0,0 +1,61 @@
+namespace GigSonar;
+
+public class Event
+{
+    private int _id;
+    private string _artist;
+    private string _venue;
+    private string _startsAt;
+    private string _endsAt;
+    private double _priceMin;
+    private double _priceMax;
+    private string _currency;
+
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    public string Artist
+    {
+        get { return _artist; }
+        set { _artist = value; }
+    }
+
+    public string Venue
+    {
+        get { return _venue; }
+        set { _venue = value; }
+    }
+
+    public string StartsAt
+    {
+        get { return _startsAt; }
+        set { _startsAt = value; }
+    }
+
+    public string EndsAt
+    {
+        get { return _endsAt; }
+        set { _endsAt = value; }
+    }
+
+    public double PriceMin
+    {
+        get { return _priceMin; }
+        set { _priceMin = value; }
+    }
+
+    public double PriceMax
+    {
+        get { return _priceMax; }
+        set { _priceMax = value; }
+    }
+
+    public string Currency
+    {
+        get { return _currency; }
+        set { _currency = value; }
+    }
+}
