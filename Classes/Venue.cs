a76a57a7504d8371992ee850e77b7579390e072f
Marko Miter <mitermarko@gmail.com>
2025-08-27 23:19:36 +0200

    Create Venue class. Add id & name properties. Add object compostion Location as LocationData.

---
+++ Classes/Venue.cs
@@ -0,0 +1,26 @@
+namespace GigSonar;
+
+public class Venue
+{
+    private int _id;
+    private string _name;
+    private Location _LocationData;
+
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public Location LocationData
+    {
+        get { return _LocationData; }
+        set { _LocationData = value; }
+    }
+}
