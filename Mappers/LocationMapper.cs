5a287794fc3eaff68c310d9b9574fdf6503a2812
Marko Miter <mitermarko@gmail.com>
2025-10-11 17:35:57 +0200

    Add necessary DTOs to convert in LocationMapper. Finish VenueMapper & LocationMapper.

--- Mappers/LocationMapper.cs
+++ Mappers/LocationMapper.cs
@@ -4,12 +4,17 @@ namespace GigSonar.Mappers;
 
 public class LocationMapper
 {
-    public static Location Convert(LocationDto locationDto)
+    public static Location Convert(LocationDto locationDto, CountryDto countryDto, CityDto cityDto, 
+        AddressDto addressDto, VenueDto venueDto)
     {
         return new Location
         {
             Latitude = locationDto.latitude,
             Longitude = locationDto.longitude,
+            CountryCode = countryDto.countryCode,
+            City = cityDto.name,
+            Address = addressDto.line1,
+            PostalCode =venueDto.postalCode,
         };
     }
 }
--- Mappers/VenueMapper.cs
+++ Mappers/VenueMapper.cs
@@ -5,9 +5,10 @@ namespace GigSonar.Mappers;
 public class VenueMapper
 {
     public static Venue Convert(VenueDto venueDto)
-    {
+    {   
         return new Venue
         {
+            ExternalId = venueDto.id,
             Name = venueDto.name,
             Url = venueDto.url,
         };
