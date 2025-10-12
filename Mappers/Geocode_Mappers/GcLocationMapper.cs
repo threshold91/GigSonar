70d485cdd40669bb38f8c2453bca3de9020cdeba
Marko Miter <mitermarko@gmail.com>
2025-10-12 19:54:13 +0200

    Mapp properties in GcLocationMapper

--- Mappers/Geocode_Mappers/GcLocationMapper.cs
+++ Mappers/Geocode_Mappers/GcLocationMapper.cs
@@ -1,6 +1,20 @@
+using GigSonar.DTOs.Geocode_DTOs.Reverse_geocode_DTOs;
+using RootDto = GigSonar.DTOs.Geocode_DTOs.Forward_geocode_DTOs.RootDto;
+
 namespace GigSonar.Mappers.Geocode_Mappers;
 
 public class GcLocationMapper
 {
-    
+    public static Location Convert(RootDto rootDto, AddressDto  addressDto)
+    {
+        return new Location
+        {
+            Latitude = rootDto.Latitude,
+            Longitude = rootDto.Longitude,
+            CountryCode = addressDto.CountryCode,
+            City = addressDto.City,
+            Address = addressDto.Road,
+            PostalCode = addressDto.Postcode
+        };
+    }
 }
