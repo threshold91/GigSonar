5217e0ebc6857918e134c5225cd8950ea4384d13
Marko Miter <mitermarko@gmail.com>
2025-10-28 15:57:05 +0100

    Copy code from main to newly created MapEvent.

---
+++ Mappers/Ticketmaster/MapEvent.cs
@@ -0,0 +1,54 @@
+using GigSonar.Classes;
+
+namespace GigSonar.Mappers.Ticketmaster;
+
+public class MapEvent
+{
+    public static Event ConvertEvent(DTOs.Ticketmaster.SearchEvents.SearchEvents.Event tmEvent)
+    {
+        var eventObject = new Event();
+        eventObject.ExternalId = tmEvent.id;
+        eventObject.Name = tmEvent.name;
+        eventObject.ArtistName = tmEvent._embedded.attractions.First().name;
+        eventObject.Genre = ConvertEventGenre(tmEvent.classifications.First());
+        eventObject.Venue = ConvertEventVenue(tmEvent._embedded.venues.First());
+        eventObject.Start = tmEvent.dates.start.dateTime;
+        //below properties not yet mapped due to api limitations
+        //event type - no such property in api response that would map to EventType enum
+        //ends - festivals are represented as single objects that appear multiple times with  different start.dateTime
+        //priceMin
+        //priceMax
+        //currency - price and currency is removed from ticketmaster discover api
+        return eventObject;
+    }
+    
+    private static Genre ConvertEventGenre(DTOs.Ticketmaster.SearchEvents.SearchEvents.Classification tmEventClassification)
+    {
+        var genre = new Genre();
+        genre.ExternalId = tmEventClassification.genre.id;
+        genre.Name = tmEventClassification.genre.name;
+        return genre;
+    }
+
+    public static Venue ConvertEventVenue(DTOs.Ticketmaster.SearchEvents.SearchEvents.Venue tmEventLocation)
+    {
+        var eventVenue = new Venue();
+        eventVenue.ExternalId = tmEventLocation.id;
+        eventVenue.Name = tmEventLocation.name;
+        eventVenue.Url = tmEventLocation.url;
+        eventVenue.LocationData = ConvertLocation(tmEventLocation);
+        return eventVenue;
+    }
+    
+    private static Classes.Location ConvertLocation(DTOs.Ticketmaster.SearchEvents.SearchEvents.Venue tmVenue)
+    {
+        var location = new Location();
+        location.Latitude = tmVenue.location.latitude;
+        location.Longitude = tmVenue.location.longitude;
+        location.CountryCode = tmVenue.country.countryCode;
+        location.City = tmVenue.city.name;
+        location.Address = tmVenue.address.line1;
+        location.PostalCode = tmVenue.postalCode;
+        return location;
+    }
+}
