65f30e8caa9334443c6ecc0526e751411afe34a0
Marko Miter <mitermarko@gmail.com>
2025-11-22 17:30:52 +0100

    Add DbContextBuilder & ModelBuilder to GigSonarContext.

--- Data/GigSonarContext.cs
+++ Data/GigSonarContext.cs
@@ -8,4 +8,17 @@ public class GigSonarContext : DbContext
     public DbSet<Event> Events { get; set; } = null!;
     public DbSet<Venue> Venues { get; set; } = null!;
     public DbSet<Artist> Artists { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=GigSonarTestDataDB.db");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
 }
