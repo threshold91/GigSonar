b09feb42ac5ceb0f603708b528a6c9206d2bd964
Marko Miter <mitermarko@gmail.com>
2025-12-20 19:11:27 +0100

    Create Subgenre class with properties.

---
+++ Classes/Subgenre.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GigSonar.Classes;
+
+[Index(nameof(ExternalId), IsUnique = true)]
+
+public class Subgenre
+{
+    private int _id;
+    private string _externalId;
+    private string _name;
+    
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    public string ExternalId
+    {
+        get { return _externalId; }
+        set { _externalId = value; }
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+}
