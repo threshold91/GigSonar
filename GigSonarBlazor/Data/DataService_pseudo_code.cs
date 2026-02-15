851ee0ba290a7442c21516a31ade4351329c9776
Marko Miter <mitermarko@gmail.com>
2026-02-15 16:00:51 +0100

    Add pseudo code from Live Call.

---
+++ GigSonarBlazor/Data/DataService_pseudo_code.cs
@@ -0,0 +1,25 @@
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace GigSonarBlazor.Data;
+
+public class DataService_pseudo_code
+{
+    /*
+    public Artist FetchArtist(string name)
+    {
+        var a = Dblookup(name);
+        if (a == null)
+            a = APILookup(name);
+            Db.artists.add(a);
+
+        return a;
+
+
+    }
+
+    private Artist APILookup(string name)
+    {
+
+    }
+    */
+}
