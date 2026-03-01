186b1abe2237e3cb29647c96cdbd547a2df563ca
Marko Miter <mitermarko@gmail.com>
2026-03-01 20:17:13 +0100

    Remove unused and commented out old code.

--- GigSonarBackend/Program.cs
+++ GigSonarBackend/Program.cs
@@ -1,18 +1,10 @@
 ﻿using System.Diagnostics;
 using GigSonarBackend.Classes;
-using GigSonarBackend.Data;
 using GigSonarBackend.Data.Services;
-using GigSonarBackend.DTOs.Ticketmaster.SearchAttractions;
-using GigSonarBackend.DTOs.Ticketmaster.SearchEvents;
-using GigSonarBackend.Mappers.Ticketmaster;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 
 namespace GigSonarBackend;
 
-using JsonSerializer = System.Text.Json.JsonSerializer;
-using Location = Classes.Location;
 using Root1 = DTOs.Ticketmaster.SearchEvents.SearchEvents.Root;
 using Root2 = DTOs.Ticketmaster.SearchVenues.Root;
 using Root3 = DTOs.Ticketmaster.SearchAttractions.Root;
@@ -25,16 +17,6 @@ class Program
     
     static async Task Main()
     {
-        //declare lists here so it is visible in both try & catch part
-        List<Event> nonValidEvents = new List<Event>();
-        //List<Event> mappedEvents = new List<Event>();
-
-        //List<Venue> mappedVenues = new List<Venue>();
-        List<Venue> nonValidVenues = new List<Venue>();
-
-        //List<Artist> mappedArtists = new List<Artist>();
-        List<Artist> nonValidArtists = new List<Artist>();
-        
             // Load config from appsettings.json
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -69,31 +51,8 @@ class Program
             
             //Convert dtoEvents to mappedEvents, add them to list
             List<Event> mappedEvents = DataService.MapAndValidateEvents(dtoEvents);
-            /*
-            foreach (var dtoEvent in dtoEvents)
-            {
-                try
-                {
-                    Event mappedEvent = MapEvent.ConvertEvent(dtoEvent);
-                    if (mappedEvent != null && mappedEvent.Validate())
-                    {
-                        mappedEvents.Add(mappedEvent);
-                    }
-                    else
-                    {
-                        nonValidEvents.Add(mappedEvent);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    nonValidEvents.Add(null);
-                    continue;
-                }
-            }*/
             
             Debug.WriteLine($"Number of valid events is: {mappedEvents.Count}");
-            Console.WriteLine($"Number of non valid events is: {nonValidEvents.Count}!");
             
             // search venue
             var testRoot2 = await DataService.GetAndDeserialize<Root2>(client, url2);
@@ -103,34 +62,8 @@ class Program
             
             //Convert dtoVenues to mappedVenues, add them to list
             List<Venue> mappedVenues = DataService.MapAndValidateVenues(dtoVenues);
-            /*
-            foreach (var dtoVenue in dtoVenues)
-            {
-                try
-                {
-                    Venue mappedVenue = MapVenue.ConvertVenue(dtoVenue);
-                        
-                    if (mappedVenue != null && mappedVenue.Validate() && mappedVenue.LocationData.Validate())
-                    {
-                        mappedVenue.LocationData.PostalCode = Location.SanitizePostalCode(mappedVenue.LocationData.PostalCode);
-                        mappedVenues.Add(mappedVenue);
-                    }
-                    else
-                    {
-                        nonValidVenues.Add(mappedVenue);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    nonValidVenues.Add(null);
-                    continue;
-                }
-            }*/
-                
             
             Console.WriteLine($"Number of valid venues is: {mappedVenues.Count}");
-            Console.WriteLine($"Number of non valid venues is: {nonValidVenues.Count}!");
             
             // search artist
             var testRoot3 = await DataService.GetAndDeserialize<Root3>(client, url3);
@@ -140,205 +73,11 @@ class Program
             
             //Convert dtoAttractions to mappedArtists, add them to list
             var mappedArtists = DataService.MapAndValidateArtists(dtoAttractions);
-            /*
-            foreach (var dtoAttraction in dtoAttractions)
-            {
-                try
-                {
-                    Artist mappedArtist = MapArtist.ConvertArtist(dtoAttraction);
-                    if (mappedArtist != null && mappedArtist.Validate())
-                    {
-                        mappedArtists.Add(mappedArtist);
-                    }
-                    else
-                    {
-                        nonValidArtists.Add(mappedArtist);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    nonValidArtists.Add(null);
-                    continue;
-                }
-            }*/
             
             Console.WriteLine($"Number of valid artists is: {mappedArtists.Count}");
-            Console.WriteLine($"Number of non valid artists is: {nonValidArtists.Count}!");
             
             DataService.SaveNewVenues(mappedVenues);
             DataService.SaveNewArtists(mappedArtists);
             DataService.SaveNewEvents(mappedEvents);
-            /*
-            using (var db = new GigSonarContext())
-            {
-                
-                var existingVenues = db.Venues.ToList();
-                var newVenues = new List<Venue>();
-                var locationByExternalId = db.Locations.AsTracking()
-                    .ToDictionary(l => l.ExternalId);
-
-                foreach (var venue in mappedVenues)
-                {
-                    var location = venue.LocationData;
-                
-                    if (!location.Validate())
-                    {
-                        Console.WriteLine($"Skipping location {location.ExternalId} – invalid");
-                        continue;
-                    }
-                    
-                    if (!existingVenues.Any(v => v.ExternalId == venue.ExternalId))
-                    {
-                        var locationExtId = venue.LocationData.ExternalId;
-
-                        if (locationByExternalId.TryGetValue(locationExtId, out var existingLocation))
-                        {
-                            venue.LocationData = existingLocation; //points to existing row
-                        }
-                        else
-                        {
-                            db.Locations.Add(venue.LocationData); //new location
-                            locationByExternalId[locationExtId] = venue.LocationData;
-                        }
-                        
-                        existingVenues.Add(venue);
-                        newVenues.Add(venue);
-                    }
-                    
-                }
-                db.Venues.AddRange(newVenues);
-                db.SaveChanges();
-                
-                var exsistingArtists = db.Artists.ToList();
-                var newArtists = new List<Artist>();
-                var genreByExternalId = db.Genres.AsTracking()
-                    .ToDictionary(g => g.ExternalId);
-                var subGenreByExternalId = db.SubGenres.AsTracking()
-                    .ToDictionary(g => g.ExternalId);
-                
-                foreach (var artist in mappedArtists)
-                {
-                    if (!exsistingArtists.Any(a => a.ExternalId == artist.ExternalId))
-                    {
-                        var genreExtId = artist.Genre.ExternalId;
-                        var subGenreExtId = artist.subGenre.ExternalId;
-
-                        if (genreByExternalId.TryGetValue(genreExtId, out var existingGenre))
-                        {
-                            artist.Genre = existingGenre;   // points to existing row
-                        }
-                        else
-                        {
-                            db.Genres.Add(artist.Genre);    // new genre
-                            genreByExternalId[genreExtId] = artist.Genre;
-                        }
-                        
-                        if (subGenreByExternalId.TryGetValue(subGenreExtId, out var existingSubGenre))
-                        {
-                            artist.subGenre = existingSubGenre;   // points to existing row
-                        }
-                        else
-                        {
-                            db.SubGenres.Add(artist.subGenre);    // new subgenre
-                            subGenreByExternalId[subGenreExtId] = artist.subGenre;
-                        }
-                        
-                        exsistingArtists.Add(artist);
-                        newArtists.Add(artist);
-                    } 
-                } 
-                
-                db.Artists.AddRange(newArtists);
-                db.SaveChanges();
-                
-                var existingEvents = db.Events.ToList();
-                var newEvents = new List<Event>();
-                var venueByExternalId = db.Venues.AsTracking()
-                    .ToDictionary(v => v.ExternalId);
-                var artistByExternalId = db.Artists.AsTracking()
-                    .ToDictionary(a => a.ExternalId);
-                
-                foreach (var ev in mappedEvents)
-                {
-                    if (!existingEvents.Any(e => e.ExternalId == ev.ExternalId))
-                    {
-                        var genreExtId = ev.Genre.ExternalId;
-                        var subGenreExtId = ev.SubGenre.ExternalId;
-                        var venueExtId = ev.Venue.ExternalId;
-                        var artistExtId = ev.Performer.ExternalId;
-                        var eventArtistGenreExtId = ev.Performer.Genre.ExternalId;
-                        var eventArtistSubGenreExtId = ev.Performer.subGenre.ExternalId;
-                        var genreByExternalId = db.Genres.AsTracking()
-                            .ToDictionary(g => g.ExternalId);
-                        var subGenreByExternalId = db.SubGenres.AsTracking()
-                            .ToDictionary(g => g.ExternalId);
-                        
-                        if (genreByExternalId.TryGetValue(genreExtId, out var existingGenre))
-                        {
-                            ev.Genre = existingGenre; // points to existing row
-                        }
-                        else
-                        {
-                            db.Genres.Add(ev.Genre); // new genre
-                            genreByExternalId[genreExtId] = ev.Genre; 
-                        }
-                        
-                        if (subGenreByExternalId.TryGetValue(subGenreExtId, out var existingSubGenre))
-                        {
-                            ev.SubGenre = existingSubGenre; // points to existing row
-                        }
-                        else
-                        {
-                            db.SubGenres.Add(ev.SubGenre); //new subgenre
-                            subGenreByExternalId[subGenreExtId] = ev.SubGenre;
-                        }
-
-                        if (venueByExternalId.TryGetValue(venueExtId, out var existingVenue))
-                        {
-                            ev.Venue = existingVenue; // points to existing row
-                        }
-                        else
-                        {
-                            db.Venues.Add(ev.Venue); //new venue
-                            venueByExternalId[venueExtId] = ev.Venue;
-                        }
-
-                        if (genreByExternalId.TryGetValue(eventArtistGenreExtId, out var duplicateGenre))
-                        {
-                            ev.Performer.Genre = duplicateGenre;    // points to existing row
-                        }
-                        else
-                        {
-                            genreByExternalId[eventArtistGenreExtId] = ev.Performer.Genre;  // new Genre
-                        }
-
-                        if (subGenreByExternalId.TryGetValue(eventArtistSubGenreExtId, out var duplicateSubGenre))
-                        {
-                            ev.Performer.subGenre = duplicateSubGenre;  // points to existing row
-                        }
-                        else
-                        {
-                            subGenreByExternalId[eventArtistSubGenreExtId] = ev.Performer.subGenre; // new subGenre
-                        }
-
-                        if (artistByExternalId.TryGetValue(artistExtId, out var exsistingArtist))
-                        {
-                            ev.Performer = exsistingArtist; // points to existing row
-                        }
-                        else
-                        {
-                            artistByExternalId[artistExtId] = ev.Performer; // new artist
-                        }
-                        existingEvents.Add(ev);
-                        newEvents.Add(ev);
-                    }
-                }
-                
-                db.Events.AddRange(newEvents);
-                db.SaveChanges();
-                Console.WriteLine("Data saved to database."); 
-            }*/
-            
     }
 }
