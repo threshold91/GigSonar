2f4d599d0de4831bcb0fc815570aae6b5c00b0cc
Marko Miter <mitermarko@gmail.com>
2026-06-30 21:42:54 +0200

    Fix folder location of geolocation.js

---
+++ GigSonarBlazor/wwwroot/js/geolocation.js
@@ -0,0 +1,25 @@
+window.getUserLocation = function () {
+    return new Promise((resolve, reject) => {
+        if (!navigator.geolocation) {
+            reject("Geolocation is not supported by this browser.");
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                resolve({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            },
+            error => {
+                reject(error.message);
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 0
+            }
+        );
+    });
+};
